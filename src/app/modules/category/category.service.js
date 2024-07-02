f2b42bcc65bf86f781788daada51af10d0663d0e
Nadir Hossain <nadirhossain336@gmail.com>
2024-07-02 18:16:04 +0600

    feat: :sparkles: category api

---
+++ src/app/modules/category/category.service.js
@@ -0,0 +1,38 @@
+const {StatusCodes} = require("http-status-codes");
+const ApiError = require("../../../../errors/ApiError");
+const unlinkFile= require("../../../../util/unlinkFile");
+const Category = require("./category.model");
+
+exports.createCategory= async(payload)=>{
+    const category = await Category.create(payload);
+    if(!category){
+        throw new ApiError(StatusCodes.BAD_REQUEST, "Field to create category");
+    }
+    return;
+}
+
+exports.deleteCategory= async(id)=>{
+    //delete from folder
+    const isCategoryExist = await Category.findById({_id: id});
+    console.log("image", isCategoryExist?.image)
+    unlinkFile(isCategoryExist?.image);
+
+    //delete from database
+    await Category.findByIdAndDelete(id);
+    return;
+}
+
+exports.getCategory= async(id)=>{
+    const category = await Category.find({});
+    return category;
+}
+
+exports.updateCategory= async(id)=>{
+    //delete from folder
+    const isCategoryExist = await Category.findById({_id: id});
+    unlinkFile(isCategoryExist?.image);
+
+    //delete from database
+    await Category.findByIdAndDelete(id);
+    return;
+}
